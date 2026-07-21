f1ac7f7c17fe410a8288dd1eb4de0040f8cac31a
Will Santos <wsantos@svi.com.ph>
2026-07-21 13:23:14 +0800

    build: fix class specification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "solitaire.solver.App"
+    mainClass = "solitaire.App"
 }
 
 // tasks.named<JavaCompile>("compileJava") {
