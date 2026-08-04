7fbf6bc6caaf404e37f9fe286eb213a0867a3f67
W. Santos <w.santos.gg@gmail.com>
2026-08-04 23:56:26 +0800

    feat: add autoskip choice

--- app/src/main/java/solitaire/App.java
+++ app/src/main/java/solitaire/App.java
@@ -8,13 +8,23 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Starting deck");
         Game game = new Game();
+        String autoSkip = "" ;
+        do {
+            System.out.print("Autoskip to end (Y/n): ");
+            autoSkip = scanner.nextLine();
+        } while (!autoSkip.equals("Y") && !autoSkip.equals("n"));
 
         GameDisplay display = GameDisplay.withBoard(game);
-        // display.setScanner(scanner);
+        if(autoSkip.equals("n")) {
+            display.setScanner(scanner);
+        }
         
         System.out.println("Starting the game GLHF");
+        System.out.println("Starting state");
         display.displayState();
+        scanner.nextLine();
 
         boolean win = game.play();
 
